0833c5122c245cac4e7ea2c75be4efde8f1ac44d
Diego Hernandez <Diego Hernandez@DESKTOP-5U9H0DS>
2021-08-09 15:03:53 -0400

    Creation of respositories and user service. Close #4

--- SBApp/src/main/java/com/example/demo/controller/UserController.java
+++ SBApp/src/main/java/com/example/demo/controller/UserController.java
@@ -1,20 +1,36 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.service.UserService;
 
 @Controller
 @EnableAutoConfiguration
 public class UserController {
 
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	RoleRepository roleRepository;
+	
 	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
 	
 	@GetMapping("/userForm")
-	public String getUserForm() {
+	public String getUserForm(Model model) {
+		model.addAttribute("userForm", new User());
+		model.addAttribute("userList", userService.getAllUsers());
+		model.addAttribute("roles",roleRepository.findAll());
+		model.addAttribute("listTab","active");
 		return "user-form/user-view";
 	}	
 }
